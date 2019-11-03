e8d16281f736c13318597e12d793e81c657f6e4e
Saki-0102 <saki030102@gmail.com>
2019-11-03 18:03:22 +0900

    바탕화면 암호화 및 복호화

---
+++ EncDesktop/setup.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+import sys
+
+buildOptions = dict(packages = ["sys", "os", "Crypto.Cipher", "Crypto.Hash", "glob"],  # 1
+	excludes = [])
+
+exe = [Executable("Enc.py")]  # 2
+
+# 3
+setup(
+    name='EncTest',
+    version = '0.1.1',
+    author = "LYE",
+    description = "Project",
+    options = dict(build_exe = buildOptions),
+    executables = exe
+)
